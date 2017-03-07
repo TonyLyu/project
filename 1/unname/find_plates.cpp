af57eb5b72f5f21c8c41a054ba6e4a60285f2fa9
TonyLyu <hdyzlh@gmail.com>
2017-03-06 20:10:02 -0500

    Add files via upload

--- 1/unname/find_plates.cpp
+++ 1/unname/find_plates.cpp
@@ -1,12 +1,10 @@
 
-#include <stdio.h>
-#include <iostream>
-#include <vector>
-
 #include "opencv2/objdetect/objdetect.hpp"
-#include "opencv2/imgproc/imgproc.hpp"
-#include "opencv2/core/core.hpp"
-#include "opencv2/ml/ml.hpp"
+ #include "opencv2/highgui/highgui.hpp"
+ #include "opencv2/imgproc/imgproc.hpp"
+
+ #include <iostream>
+ #include <stdio.h>
 
 using namespace cv;
 using namespace std;
@@ -17,11 +15,14 @@ using namespace std;
  {
  	CascadeClassifier plates_lbp;
  	String trainfile = "us.xml";
-
+  if(!plates_lbp.load(trainfile))
+  {
+    printf("error");
+  }
  	equalizeHist(frame, frame);
  	Size min_plate_size(100, 100);
  	Size max_plate_size(maxWidth, maxHeight);
- 	vector<Rect> plates;
+ 	std::vector<Rect> plates;
  	plates_lbp.detectMultiScale( frame, plates, 1.1, 3,
                                       CV_HAAR_DO_CANNY_PRUNING,
                                       //0|CV_HAAR_SCALE_IMAGE,
@@ -29,12 +30,12 @@ using namespace std;
  	return plates;
  }
 
- int main(char** argv)
+ int main(int argc,char** argv)
  {
  	Mat image;
  	Mat image_gray;
  	Mat frame_gray;
-    image = imread( argv[1], 1);
+  image = imread( argv[1], 1);
 
     if ( !image.data )
     {
@@ -47,13 +48,22 @@ using namespace std;
     }
     else
     {
-      image.copyTo(frame_gray);
+      image.copyTo(image_gray);
     }
-    int w = frame_gray.size().width;
-    int h = frame_gray.size().height;
-    vector<Rect> allRegions = find_plates(frame_gray, w, h);
-    printf(allRegions)
+    int w = image_gray.size().width;
+    int h = image_gray.size().height;
+    vector<Rect> allRegions = find_plates(image_gray, w, h);
+    for(size_t i = 0; i < allRegions.size(); i++)
+    {
+      printf("12");
+      Point center(allRegions[i].x + allRegions[i].width*0.5,allRegions[i].y+allRegions[i].height*0.5 );
+      ellipse( image, center, Size( allRegions[i].width*0.5, allRegions[i].height*0.5), 0, 0, 360, Scalar( 255, 0, 255 ), 4, 8, 0 );
+
+    }
+    imshow("123",image);
+    waitKey(0);
+    //printf(allRegions)
     return 0;
- }
+  }
 
 
